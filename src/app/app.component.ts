187453485b8f935d9a991575aacc874b2ced4b16
Rafael Ruiz <rafaelmostoles24595@gmail.com>
2024-04-23 21:03:46 +0200

    [FEATURE] Project restructured:
    - RustDbManagerService basic structure created.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './header/header.component';
-import { BodyComponent } from './body/body.component';
+import { HeaderComponent } from './pages/index/header/header.component';
+import { BodyComponent } from './pages/index/body/body.component';
 
 @Component({
   selector: 'app-root',
